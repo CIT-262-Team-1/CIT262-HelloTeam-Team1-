2b8eeaa0c55236ce9aa2c8995b65969ef05d29c3
waheednazeer <waheed.nazeer@hotmail.com>
2024-01-31 21:17:16 +0500

    waheed branch added

--- NodeJSExample/helloworld.js
+++ NodeJSExample/helloworld.js
@@ -1,4 +1,4 @@
-let our_names = ["Caden", "Josh", "Luke", "Cameron"]
+let our_names = ["Caden", "Josh", "Luke", "Cameron", "Waheed"]
 
 for (const name of our_names) {
     console.log("Hello " + name);
